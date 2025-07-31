b01bdf843cf73a4e0dba5cab89bcaadff732ab4d
Aj-Bernal <axceljoh.bernal@cvsu.edu.ph>
2025-07-31 14:34:29 +0800

    Update and rename cs to ex1.cs

--- Exercise/ex1.cs
+++ Exercise/ex1.cs
@@ -1,4 +1,17 @@
-string projectName = "ACME";
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Exercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        string projectName = "ACME";
 string message = $@"View English output: 
     c:\Exercise\{projectName}\data.txt";
 string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
@@ -6,3 +19,6 @@ string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\
 Console.WriteLine(message + "\n");
 Console.WriteLine($@"{russianMessage}: 
     c:\Exercise\{projectName}\ru_RU\data.txt");
+        }
+    }
+}
