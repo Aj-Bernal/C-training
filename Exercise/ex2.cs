51c70014fc196f61d83e7b576ead9f04ca2a71cb
Aj-Bernal <axceljoh.bernal@cvsu.edu.ph>
2025-07-31 16:12:47 +0800

    Create ex2.cs

---
+++ Exercise/ex2.cs
@@ -0,0 +1,19 @@
+int value = 1;
+
+value = value + 1;
+Console.WriteLine("First increment: " + value); // 2
+
+value += 1;
+Console.WriteLine("Second increment: " + value); // 3
+
+value++;
+Console.WriteLine("Third increment: " + value); // 4
+
+value = value - 1;
+Console.WriteLine("First decrement: " + value); // 3
+
+value -= 1;
+Console.WriteLine("Second decrement: " + value); // 2
+
+value--;
+Console.WriteLine("Third increment: " + value); //1
