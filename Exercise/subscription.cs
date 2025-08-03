5268147848dd202e3b9b42317d5403240804a581
Aj-Bernal <axceljoh.bernal@cvsu.edu.ph>
2025-08-03 17:46:54 +0800

    Create subscription.cs

---
+++ Exercise/subscription.cs
@@ -0,0 +1,33 @@
+using System.Formats.Asn1;
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration <= 10)
+{
+    if (daysUntilExpiration == 0)
+    { 
+        System.Console.WriteLine("Your subscription has expired.");
+    }
+    else if (daysUntilExpiration <= 1)
+    {
+        discountPercentage = 20;
+        System.Console.WriteLine($@"Your subscription expires within a day!
+Renew now and save {discountPercentage}!");
+    }
+    else if (daysUntilExpiration <= 5)
+    {
+        discountPercentage = 10;
+        System.Console.WriteLine($@"Your subscription expires in {daysUntilExpiration} days.
+Renew now and save {discountPercentage}!");
+    }
+    else
+    {
+        System.Console.WriteLine("Your subscription will expire soon. Renew now!");
+    }    
+}
+else
+{
+
+}
